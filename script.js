66e969de07a473317b5a3b64f434848919f49222
Zack <zackbrisson@hotmail.com>
2026-07-26 03:14:52 -0500

    Add book form and display submitted titles

---
+++ script.js
@@ -0,0 +1,42 @@
+const bookForm = document.getElementById("book-form");
+const titleInput = document.getElementById("title-input");
+const authorInput = document.getElementById("author-input");
+const pagesInput = document.getElementById("pages-input");
+const genreInput = document.getElementById("genre-input");
+const subGenreInput = document.getElementById("sub-genre-input");
+const bookCoverInput = document.getElementById("book-cover-input");
+const seriesInput = document.getElementById("series-input");
+const readingStatusInput = document.getElementById("reading-status-input");
+const bookList = document.getElementById("book-list");
+
+//array for storing books added
+const books = [];
+
+
+// This code shows what happens when form is submitted
+  bookForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+const newBook = {
+    title: titleInput.value,
+    author: authorInput.value,
+    pages: pagesInput.value,
+    genre: genreInput.value,
+    subGenre: subGenreInput.value,
+    bookCover: bookCoverInput.value,
+    series: seriesInput.value,
+    readingStatus: readingStatusInput.value
+};
+  
+books.push(newBook);
+  bookList.innerHTML = "";
+
+  //Loop for adding book titles to page
+  books.forEach(function(book) {
+    bookList.innerHTML += book.title + "<br>";
+});
+
+  console.log(newBook);
+  console.log("Book submitted");
+  console.log(books);
+});
